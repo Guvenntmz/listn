faedac57037d2b0f163a951908b4efb50f0daded
guven <guvenntmz@gmail.com>
2021-02-11 17:39:50 +0300

    2

--- src/shared/Cards.svelte
+++ src/shared/Cards.svelte
@@ -1,10 +1,6 @@
 <script>
     import { adminPlaylists } from '../scripts/stores';
 
-
-    export let playlistName = '';
-    export let mood = '';
-    export let songs = [];
     
     let playlists = [];
     
@@ -20,14 +16,24 @@
     <!-- Additional required wrapper -->
     <div class="swiper-wrapper">
         <!-- Slides -->
-        <div class="swiper-slide">Slide 1</div>
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
+        {#each playlists as playlist}
+            <div class="swiper-slide">
+                <div class="card">
+                    <div class='card-header text-center'>Up For some {playlist.mood} music?</div>
+                    <div class="card-body">
+                        <h1 class='card-title text-center text-truncate'>{playlist.playlistName}</h1>
+                        {#each playlist.songs as song}
+                            <p class='card-text text-truncate'>{song}</p>
+                        {/each}
+                    </div>
+                    <div class="card-footer text-center">by Admin</div>
+                </div>
+            </div>
+        {/each}
+        <!-- If we need scrollbar -->
+        <div class="swiper-scrollbar"></div>
     </div>
-    <!-- If we need scrollbar -->
-    <div class="swiper-scrollbar"></div>
 </div>
-  
 
 
 <style>
